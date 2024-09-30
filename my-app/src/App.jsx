fa54e9928c025005833d9b2fe86bab9b0d19c2b4
Harsha <harshadevadiga6289@gmail.com>
2024-09-30 09:58:53 +0530

    Resolved merge conflicts

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,6 +5,9 @@ function App() {
   return (
    <div>
     <Home/>
+    <p1>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum consectetur, dolore ratione quis consequuntur alias accusantium. Commodi sapiente vero quos culpa odit, molestiae maiores reprehenderit placeat perspiciatis a! Vero, beatae.
+    </p1>
    </div>
   )
 }
