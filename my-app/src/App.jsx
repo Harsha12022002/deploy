5ea8c890c075344356d6701e9cbf20723d2611e5
Harsha12022002 <harshadevadiga6289@gmail.com>
2024-10-23 17:31:59 +0530

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -11,17 +11,8 @@ import Buying from './Buying';
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   return (
-    <Router>
+   
       <Navbar />
-      <Routes>
-        <Route path='/Pc' element={<Pc/>}></Route>
-        <Route path='/Cart' element={<Cart/>}></Route>
-        <Route path='/' element={<Login setIsAuthenticated={setIsAuthenticated} />} />
-        <Route path='/sign' element={<Signup setIsAuthenticated={setIsAuthenticated} />} />
-        <Route path='/Buying' element={<Buying/>}/>
-        <Route path='/home' element={isAuthenticated ? <Home /> : <Login setIsAuthenticated={setIsAuthenticated} />} />
-      </Routes>
-    </Router>
   );
 }
 
