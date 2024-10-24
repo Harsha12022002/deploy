008237c4ff57e87ac8cfd669b99a6787eefdf366
Harsha12022002 <harshadevadiga6289@gmail.com>
2024-10-24 19:00:01 +0530

    Update vite.config.js

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // default is 'dist', but you can customize it here if needed
+    outDir: 'dist' 
   }
 })
